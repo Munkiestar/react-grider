c6f86ed69957c4ced4ff433bb9ab0757da0ca911
Igor <munkieryder@gmail.com>
2020-05-28 11:43:30 +0200

    adding faker js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import './App.css';
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-hi there
+            <div className='ui container comments'>
+                <div className="comment">
+                    <a href="/" className='avatar'>
+                        <img src="" alt="avatar"/>
+                    </a>
+                    <div className="content">
+                        <a href="/" className='author'>
+                            Sam
+                        </a>
+                    </div>
+                    <div className="metadata">
+                        <span className='date'>Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog Post!</div>
+                </div>
             </div>
         );
     }
