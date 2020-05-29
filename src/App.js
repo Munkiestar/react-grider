a3f24b08f6deb9c50af647588dc67e8f0b720030
Igor <munkieryder@gmail.com>
2020-05-29 11:14:55 +0200

    adding fontawesome icons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import SeasonDisplay from "./SeasonDisplay/SeasonDisplay";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {lat: null, errorMessage: ''};
+    // }
+    state = {lat: null, errorMessage: ''};
 
-        this.state = {lat: null, errorMessage: ''};
-
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({lat: position.coords.latitude}),
             err => this.setState({errorMessage: err.message})
         );
-
     }
+
 
     render() {
         if (this.state.errorMessage && !this.state.lat) {
@@ -20,7 +23,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div> Latitude={this.state.lat} </div>;
+            return <SeasonDisplay latitude={this.state.lat}/>;
         }
 
         return <div> "Please accept location request" </div>;
--- src/SeasonDisplay/SeasonDisplay.js
+++ src/SeasonDisplay/SeasonDisplay.js
@@ -1,13 +1,37 @@
-import React, {Component} from 'react';
+import React from 'react';
+// import './SeasonDisplay.css';
 
-class SeasonDisplay extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        );
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brr, it is chilly',
+        iconName: 'snowflake'
     }
 }
 
-export default SeasonDisplay;
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat < 0 ? 'winter' : 'summer';
+    }
+}
+
+const seasonDisplay = props => {
+    const season = getSeason(props.latitude, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    return (
+        <div className={`season-display ${season}`}>
+            < i className={`fas fa-${iconName} icon-left fa-7x`}/>
+            <h1>{text}</h1>
+            < i className={`fas fa-${iconName} icon-right fa-7x`}/>
+        </div>
+    );
+}
+
+
+export default seasonDisplay;
