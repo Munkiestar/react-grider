bb37103eef2e94e6896fd953dc083b222afe80c9
Igor <munkieryder@gmail.com>
2020-05-30 12:49:55 +0200

    adding axios and unsplash api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css'
 import SearchBar from "./components/SearchBar";
 
-function App() {
-    return (
-        <div className='ui container' style={{marginTop: '25px'}}>
-            <SearchBar/>
-        </div>
-    );
+class App extends Component {
+
+    onSearchSubmit(term) {
+        console.log(term)
+        // Aw081Q4CfKSRjJE0wTSXIJKDmRkH_xilWenNlmR8yKg
+    }
+
+    render() {
+        return (
+            <div className='ui container' style={{marginTop: '25px'}}>
+                <SearchBar handleSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,27 @@
 import React, {Component} from "react";
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
 
-    state = {
-        term: ''
+        this.state = {
+            term: ''
+        }
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+
+        this.props.handleSubmit(this.state.term);
     }
 
     render() {
 
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={this.handleFormSubmit}>
                     <div className='field'>
                         <label htmlFor="">Image Search</label>
                         <input
