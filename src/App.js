1d506bdc0b8850ba8ba55532091a2340f47ef17d
Igor <munkieryder@gmail.com>
2020-06-03 15:29:28 +0200

    youtube search videos

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
         selectedVideo: null
     }
 
+    // when app first loads it shows videos of 'usa sport today
+    componentDidMount() {
+        this.handleTermSubmit('usa sport today')
+    }
 
     handleTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -21,22 +25,37 @@ class App extends Component {
             }
         });
 
-        this.setState({videos: response.data.items})
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
     }
 
+
     handleVideoSelect = video => {
-        this.setState({selectedVideo: video})
+        this.setState(
+            {selectedVideo: video}
+        )
     }
 
     render() {
         return (
             <div className='ui container' style={{marginTop: '35px'}}>
                 <SearchBar onFormSubmit={this.handleTermSubmit}/>
-                <VideoDetail video={this.state.selectedVideo}/>
-                <VideoList
-                    videos={this.state.videos}
-                    onVideoSelect={this.handleVideoSelect}
-                />
+                <div className='ui grid'>
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <div className="ui row"><VideoDetail video={this.state.selectedVideo}/>
+                            </div>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                                videos={this.state.videos}
+                                onVideoSelect={this.handleVideoSelect}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -3,7 +3,27 @@ import React from "react";
 const VideoDetail = ({video}) => {
 
     if (!video) {
-        return <div>Loading...</div>
+        return (
+            <div className="ui segment">
+                <div className="ui active transition visible dimmer">
+                    <div className="content">
+                        <div className="ui massive text loader">Loading</div>
+                    </div>
+                </div>
+                <img
+                    src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png"
+                    className="ui image"
+                />
+                <img
+                    src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png"
+                    className="ui image"
+                />
+                <img
+                    src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png"
+                    className="ui image"
+                />
+            </div>
+        )
     }
 
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
@@ -11,7 +31,7 @@ const VideoDetail = ({video}) => {
     return (
         <div>
             <div className="ui embed">
-                <iframe src={videoSrc}></iframe>
+                <iframe title='video player' src={videoSrc}></iframe>
             </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
